a961dd1fe376efdf1b5d0a0d6b900d38f61515ac
Doanvu2k <vdd.ytnd@gmail.com>
2021-12-03 16:34:21 +0700

    Chinh sua trang detail cua tour

--- BTL_10/App_Start/RouteConfig.cs
+++ BTL_10/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace BTL_10
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
